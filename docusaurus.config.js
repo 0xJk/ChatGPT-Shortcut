a946ff640c5d192c6051953d2e8e936f136c151d
ChefKang <0xjunkie@gmail.com>
2023-09-18 19:53:57 +1000

    ipdate

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,10 +14,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://www.chefkang.xyz/gpt-tools",
+  url: "https://www.chefkang.xyz/gpt-tools/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/gpt-tools",
+  baseUrl: "/gpt-tools/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
